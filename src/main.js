7e2497152f22fff6f251bb3e356224dce33ffdf6
tronghia <ttnghia5112002@gmail.com>
2024-04-04 22:09:14 +0700

    Tao trang cap nhat lien he

--- src/main.js
+++ src/main.js
@@ -8,4 +8,4 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import router from "./router";
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount('#app');
--- src/router/index.js
+++ src/router/index.js
@@ -13,10 +13,23 @@ const routes = [
         name: "notfound",
         component: () => import("@/views/NotFound.vue"),
     },
+
+    {
+        path: "/contacts/:id",
+        name: "contact.edit",
+        component: () => import("@/views/ContactEdit.vue"),
+        props: true, // Truyền các biến trong $route.params vào làm props
+    },
+
+    // {
+    //     path: "/contacts",
+    //     name: "contact.add",
+    //     component: () => import("@/views/ContactAdd.vue"),
+    // },
 ];
 
 const router = createRouter({
-    history: createWebHistory (import.meta.env.BASE_URL),
+    history: createwebнistory (import.meta.env.BASE_URL),
     routes,
 });
 
